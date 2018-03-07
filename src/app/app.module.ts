741c68c821d59ce39ecffbdbf45e40da6d03df2a
fmaslim <franky.maslim@yahoo.com>
2018-03-07 18:08:19 -0500

    Added hello-world, user-item, user-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
+],
   imports: [
     BrowserModule
   ],
